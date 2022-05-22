c40a2269cdc1df5c2805a779931d344550679ea9
DumasOlivier <dumas.olivier@outlook.fr>
2022-05-22 18:46:26 +0200

    create albums

--- contracts/Tokenizer.sol
+++ contracts/Tokenizer.sol
@@ -4,28 +4,41 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Tokenizer {
-    uint16 public constant maxNftSupplyPerAlbum = 10000;
+    uint256 public constant maxNftSupplyPerAlbum = 10000;
     uint256 public musiciansCounter = 0;
+    uint256 public albumsCounter = 0;
     string public registerFee = '0.01';
     struct Musician {
         string name;
+        uint256 albumsCounter;
     }
-    mapping(address => Musician) public addressToMusicians;
+    mapping(address => Musician) public addressToMusician;
     struct Album {
         string title;
-        uint16 maxNftSupply;
+        uint256 maxNftSupply;
     }
-    mapping(uint256 => Album) public albumToMusician;
+    mapping(address => Album[]) public addressToAlbums;
 
     // Events
-    event RegisterMusician(address indexed _from, string name);
+    event RegisterMusician(address indexed _from, string _name);
+    event CreateAlbum(address indexed _from, string _title, uint256 _maxNftSupply);
 
     constructor() {}
 
+    function createAlbum(string memory _title, uint256 _maxNftSupply) public {
+        // TODO: Require that the musician is registered.
+        require(_maxNftSupply <= maxNftSupplyPerAlbum);
+
+        addressToAlbums[msg.sender].push(Album(_title, _maxNftSupply));
+        addressToMusician[msg.sender].albumsCounter ++;
+        albumsCounter++;
+        emit CreateAlbum(msg.sender, _title, _maxNftSupply);
+    }
+
     function registerMusician(string memory _name) public {
-        require(bytes(addressToMusicians[msg.sender].name).length == 0, "You have already created a musician.");
-        // TODO : Require musician to have paid the register fee.
-        addressToMusicians[msg.sender] = Musician(_name);
+        require(bytes(addressToMusician[msg.sender].name).length == 0, "You have already created a musician.");
+
+        addressToMusician[msg.sender] = Musician(_name, 0);
         musiciansCounter++;
         emit RegisterMusician(msg.sender, _name);
     }
@@ -35,6 +48,6 @@ contract Tokenizer {
     }
 
     function getMusician(address _musicianAddress) public view returns(Musician memory){
-        return addressToMusicians[_musicianAddress];
+        return addressToMusician[_musicianAddress];
     }
 }
